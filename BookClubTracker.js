d8842dd7899e70f4a04a01e9eb84f56d135887f4
Leo <39741041+leohumnew@users.noreply.github.com>
2023-10-11 19:59:55 +0100

    0.2 - Club add panel
    - Adding book clubs is now done with a popup panel, make it much more intuitive

--- BookClubTracker.js
+++ BookClubTracker.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Book Club Tracker
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Add a panel to the WK Readers page to track book club progress
 // @author       leohumnew
 // @match        https://www.wanikani.com/*
@@ -21,7 +21,7 @@
     }
 
     // ------------------ MAIN SCRIPT ------------------
-    // Check if we're on the /readers or dashboard page
+    // ----- DASHBOARD -----
     if (location.pathname == "/dashboard" || location.pathname == "/") {
 
         // Create and add styles to page
@@ -41,6 +41,7 @@
         newButtonLi.appendChild(newButton);
         parentElement.prepend(newButtonLi);
         
+    // ----- READERS -----
     } else if (location.pathname == "/readers") {
 
         let bookClubs = loadBookClubs();
@@ -57,7 +58,20 @@
         style.innerHTML += ".book-club-week--completed h3 { font-weight: 600; color: color-mix(in srgb, var(--color-correct-light) 50%, #0000); }";
         style.innerHTML += ".book-club-week--active { font-weight: 600; border: 1px solid var(--color-tertiary); }";
         style.innerHTML += ".book-club-week--inactive { filter: opacity(0.5); }";
+        style.innerHTML += ".edit-popup { background-color: var(--color-menu); border-radius: 7px; padding: var(--spacing-loose); width: 50%; height: 50%; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); }";
+        style.innerHTML += ".edit-popup h2 { text-align: center; font-size: 2rem; margin-bottom: var(--spacing-loose); }";
+        style.innerHTML += ".edit-popup__form > input { margin-bottom: var(--spacing-tight); margin-left: var(--spacing-loose); padding: 3px; border-radius: 4px; }";
+        style.innerHTML += ".edit-popup__form input:focus { outline: 1px solid var(--color-tertiary); }";
+        style.innerHTML += ".edit-popup__form > label { font-weight: 600; }";
+        style.innerHTML += ".edit-popup #weeksInfo { background-color: var(--color-dashboard-panel-content-background); padding: 15px; border-radius: 7px; margin: 10px 0; }";
+        style.innerHTML += ".edit-popup #weeksInfo form { display: flex; flex-direction: row; flex-wrap: wrap; justify-content: space-between; gap: 15px; }";
+        style.innerHTML += ".edit-popup__form > button { margin: auto; cursor: pointer; width: 100%; }";
         document.head.appendChild(style);
+
+        // Get book club helper function
+        function getBookClub(title) {
+            return bookClubs.find(bookClub => bookClub.title === title);
+        }
 
         // Create a panel header
         function createPanelHeader(text, button) {
@@ -84,40 +98,39 @@
 
         // Create a book club panel
         function createBookClubPanel(bookClubInfo) {
-            let bookClubPanel = document.createElement('div');
+            let bookClubPanel = document.createElement('div'); // Create a div for the book club panel
             bookClubPanel.className = "book-club";
 
-            let bookClubHeader = document.createElement('div');
+            let bookClubHeader = document.createElement('div'); // Create the header with the title and delete button
             bookClubHeader.style.display = "flex";
             let bookClubTitle = document.createElement('a');
             bookClubTitle.className = "reader-summary__title";
             bookClubTitle.href = bookClubInfo.url;
             bookClubTitle.target = "_blank";
             bookClubTitle.innerHTML = bookClubInfo.title;
-            let deleteButton = createButton("X", function() {
+            let deleteButton = createButton("", function() {
                 if (confirm("Are you sure you want to delete this book club?")) deleteBookClub(bookClubInfo.title);
                 location.reload();
             });
-            deleteButton.className = "delete-club-button";
+            deleteButton.className = "delete-club-button wk-icon fa-regular fa-times";
             bookClubHeader.append(bookClubTitle, deleteButton);
 
-            let bookClubSubTitle = document.createElement('span');
+            let bookClubSubTitle = document.createElement('span'); // Create the subtitle with the vocab list link and active status
             bookClubSubTitle.className = "reader-summary__status";
             bookClubSubTitle.innerHTML = "<a target='_blank' href='" + bookClubInfo.vocabListUrl + "'>Vocab List</a> | ";
             bookClubSubTitle.innerHTML += bookClubInfo.active ? "<span style='color: var(--color-correct)'>Active</span>" : "<span style='color: var(--color-incorrect)'>Inactive</span>";
 
-            // Create a div for each week and its info
-            let bookClubWeeks = document.createElement('div');
+            let bookClubWeeks = document.createElement('div'); // Create a div for each week and its info
             bookClubWeeks.className = "book-club-weeks";
-            for (let i = 0; i < bookClubInfo.weeksInfo.length; i++) {
-                let week = document.createElement('div');
+            for (let i = 0; i < bookClubInfo.weeksInfo.length; i++) { // Loop over each week
+                let week = document.createElement('div'); // Create a div for the week and add a click handler to toggle the week's complete status
                 week.className = "book-club-week";
                 week.addEventListener("click", function() {
                     toggleBookClubWeek(bookClubInfo.title, i + 1);
                     location.reload();
                 });
                 
-                let weekTitle = document.createElement('h3');
+                let weekTitle = document.createElement('h3'); // Create a title and info for the week
                 weekTitle.innerHTML = "Week " + (i + 1);
 
                 let weekInfo = document.createElement('p');
@@ -138,11 +151,11 @@
                     week.className += " book-club-week--active";
                 }
                 
-                week.append(weekTitle, weekInfo);
+                week.append(weekTitle, weekInfo); // Append the title and info to the week div
                 bookClubWeeks.appendChild(week);
             }
 
-            bookClubPanel.append(bookClubHeader, bookClubSubTitle, bookClubWeeks);
+            bookClubPanel.append(bookClubHeader, bookClubSubTitle, bookClubWeeks); // Append the header, subtitle and weeks to the book club panel
             return bookClubPanel;
         }
 
@@ -174,9 +187,110 @@
             saveBookClubs();
         }
         function toggleBookClubWeek(bookClubTitle, weekNumber) {
-            let bookClub = bookClubs.find(bookClub => bookClub.title === bookClubTitle);
+            let bookClub = getBookClub(bookClubTitle);
             bookClub.weeksInfo[weekNumber - 1].completed = !bookClub.weeksInfo[weekNumber - 1].completed;
             saveBookClubs();
+        }
+
+        // Create a popup menu to add / edit a book club
+        function showBookClubEditPopup(isCreating, bookClubName) {
+            let bookClub = isCreating ? null : getBookClub(bookClubName);
+
+            let popup = document.createElement('div');
+            popup.className = "edit-popup";
+
+            let popupTitle = document.createElement('h2');
+            popupTitle.innerHTML = isCreating ? "Add Book Club" : "Edit Book Club";
+
+            // Create a container for all the text inputs, then add them with an html string to the popupForm's innerHTML
+            let popupForm = document.createElement('div');
+            popupForm.className = "edit-popup__form";
+            popupForm.innerHTML = isCreating ? "<label for='title'>Title</label><input type='text' id='title' name='title' placeholder='Title' required>" : "<label for='title'></label><h3 id='title'>" + bookClub.title + "</h2>";
+            popupForm.innerHTML += `
+                <br><label for='url'>Main Thread URL</label><input type='text' id='url' name='url' placeholder='URL' required><br>
+                <label for='vocabListUrl'>Vocab List URL</label><input type='text' id='vocabListUrl' name='vocabListUrl' placeholder='URL' required><br>
+                <label for='weeksInfo'>Weeks Info</label>
+            `;
+            let weeksInfoContainer = document.createElement('div');
+            weeksInfoContainer.id = "weeksInfo";
+            popupForm.appendChild(weeksInfoContainer);
+            let saveButton = createButton("Save", function() {
+                if (isCreating) {
+                    addBookClub(
+                        document.querySelector("#title").value,
+                        document.querySelector("#url").value,
+                        weeksInfo,
+                        document.querySelector("#vocabListUrl").value
+                    );
+                    } else {
+                        let bookClub = getBookClub(document.querySelector("#title").value);
+                        bookClub.url = document.querySelector("#url").value;
+                        bookClub.weeksInfo = weeksInfo;
+                        bookClub.vocabListUrl = document.querySelector("#vocabListUrl").value;
+                    }
+                saveBookClubs();
+                location.reload();
+            });
+            saveButton.className = "wk-button--default";
+            popupForm.appendChild(saveButton);
+
+            // Set up weeks info content
+            let weeksInfo = isCreating ? [] : bookClub.weeksInfo;
+            let newWeekForm = document.createElement('form'); // Create week adding form
+            newWeekForm.innerHTML = `
+                <label for='startPage'>Start Page</label><input type='number' id='startPage' name='startPage' placeholder='Start Page' required>
+                <label for='endPage'>End Page</label><input type='number' id='endPage' name='endPage' placeholder='End Page' required>
+                <label for='startDate'>Start Date</label><input type='date' id='startDate' name='startDate' placeholder='Start Date' required>
+                <button type='submit'>+</button>
+                <hr style="width: 100%;">
+            `;
+            newWeekForm.addEventListener("submit", function(e) {
+                e.preventDefault();
+                let newWeekInfo = { // Create new week info object, and add it to weeksInfo in the correct position according to start date (with week 1 at the start of the array)
+                    completed: false,
+                    startPage: document.querySelector("#startPage").value,
+                    endPage: document.querySelector("#endPage").value,
+                    startDate: new Date(document.querySelector("#startDate").value)
+                };
+                let index = weeksInfo.findIndex(week => new Date(week.startDate) > new Date(newWeekInfo.startDate));
+                if (index === -1) {
+                    weeksInfo.push(newWeekInfo);
+                } else {
+                    weeksInfo.splice(index, 0, newWeekInfo);
+                }
+                
+                let weeksInfoList = document.querySelector("#weeksInfo ul");
+                let weekElement = document.createElement('li'); // Create new week element, and insert it in weeksInfoList in the correct position
+                weekElement.innerHTML = "Week " + (index + 1) + " - Pages: " + newWeekInfo.startPage + " - " + newWeekInfo.endPage + " - Start Date: " + new Date(newWeekInfo.startDate).toLocaleDateString();
+                let deleteWeekButton = createButton("", function() { // Create delete button
+                    weeksInfo.splice(index, 1);
+                    weeksInfoList.removeChild(weekElement);
+                });
+                deleteWeekButton.className = "delete-week-button wk-icon fa-regular fa-times";
+                weekElement.appendChild(deleteWeekButton);
+                weeksInfoList.insertBefore(weekElement, weeksInfoList.childNodes[index]);
+
+                newWeekForm.reset();
+                document.querySelector("#startPage").focus();
+            });
+
+            let weeksInfoList = document.createElement('ul'); // Create list of weeks
+            for (let i = 0; i < weeksInfo.length; i++) {
+                let week = weeksInfo[i];
+                let weekElement = document.createElement('li');
+                weekElement.innerHTML = "Week " + (i + 1) + " - Pages: " + week.startPage + " - " + week.endPage + " - Start Date: " + new Date(week.startDate).toLocaleDateString();
+                let deleteWeekButton = createButton("", function() { // Create delete button for each week
+                    weeksInfo.splice(i, 1);
+                    weeksInfoList.removeChild(weekElement);
+                });
+                deleteWeekButton.className = "delete-week-button wk-icon fa-regular fa-times";
+                weekElement.appendChild(deleteWeekButton);
+                weeksInfoList.appendChild(weekElement);
+            }
+
+            weeksInfoContainer.append(newWeekForm, weeksInfoList);
+            popup.append(popupTitle, popupForm);
+            document.body.appendChild(popup);
         }
 
         // Wait for page and turbo to finish loading, by waiting for the turbo:load event
@@ -192,29 +306,7 @@
 
             // Create a button to add a new book club
             let newClubButton = createButton("Add Book Club", function() {
-                let title = prompt("Enter the title of the book club");
-                let url = prompt("Enter the URL of the main thread");
-                let vocabListUrl = prompt("Enter the URL of the vocab list");
-                let weeksInfo = [];
-                let weekNumber = 1;
-                while (true) {
-                    let startPage = prompt("Enter the start page of week " + weekNumber + " (or leave blank to stop)");
-                    if (startPage === "") {
-                        break;
-                    }
-                    let endPage = prompt("Enter the end page of week " + weekNumber);
-                    let startDate = null;
-                    while(startDate == null) startDate = new Date(prompt("Enter the start date of week " + weekNumber));
-                    weeksInfo.push({
-                        completed: false,
-                        startPage: startPage,
-                        endPage: endPage,
-                        startDate: startDate
-                    });
-                    weekNumber++;
-                }
-                addBookClub(title, url, weeksInfo, vocabListUrl);
-                location.reload();
+                showBookClubEditPopup(true, null);
             });
             newClubButton.className = "info-popover__button";
 
